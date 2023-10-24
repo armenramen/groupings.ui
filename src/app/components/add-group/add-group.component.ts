6d9e21e5e927606571a6500d7af144fe974fffe1
Armen Dela Cruz <delacruzrmn@gmail.com>
2023-10-25 01:59:51 +0800

    Save new group api call

--- src/app/components/add-group/add-group.component.ts
+++ src/app/components/add-group/add-group.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { catchError, of } from 'rxjs';
+import { GroupingsService } from 'src/app/services/groupings.service';
 
 @Component({
   selector: 'app-add-group',
@@ -8,17 +11,21 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddGroupComponent implements OnInit {
   groupForm!: FormGroup;
+  isSaving = false;
 
   get detailsForms() {
-    return this.groupForm.get('details') as FormArray
+    return this.groupForm.get('properties') as FormArray
   }
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private groupingService: GroupingsService,
+    private dialogRef: MatDialogRef<AddGroupComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { userId: string }) { }
 
   ngOnInit() {
     this.groupForm = this.formBuilder.group({
-      groupName: ['', [Validators.required]],
-      details: this.formBuilder.array([
+      name: ['', [Validators.required]],
+      properties: this.formBuilder.array([
         this.formBuilder.group({
           propertyName: ['', [Validators.required]],
           type: ['text', Validators.required]
@@ -33,6 +40,25 @@ export class AddGroupComponent implements OnInit {
       propertyName: ['', [Validators.required]],
       type: ['text', Validators.required]
     }))
+  }
+
+  saveNewGroup() {
+    this.isSaving = true;
+    const formValues = this.groupForm.value;
+
+    this.groupingService.saveGroup(
+      this.data.userId,
+      formValues
+    )
+      .pipe(catchError(error => of({ error })))
+      .subscribe((res: any) => {
+        this.isSaving = false;
+        if (res.error) {
+          this.dialogRef.close({ type: 'error' });
+          return;
+        }
+        this.dialogRef.close({ type: 'success', value: res });
+      })
   }
 
 }
